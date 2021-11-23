fd60e0dbe74f04703d3fc18b3487172edc965cea
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-11-23 13:38:21 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+const feet = 264;
+const hq = 42;
+
+
+function distanceFromHqInBlocks(location){
+   if(location > hq){
+return location - hq;
+   } 
+   else if(location < hq){
+       return hq - location;
+   }
+    
+}
+
+distanceFromHqInBlocks(50);
+
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * feet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distance = Math.abs(destination - start);
+    return distance * feet;
+}
+
+function calculatesFarePrice(start, destination) {
+        
+    let traveled = distanceTravelledInFeet(start, destination);
+
+    if (traveled <= 400){
+        return 0;
+    } else if (traveled > 400 && traveled <= 2000){
+        return (traveled - 400) * .02;
+    }
+    else if ( traveled > 2000 && traveled < 2500){
+        return 25;
+    }
+    else if (traveled > 2500){
+        return 'cannot travel that far';
+    }
+
+}
+
+
